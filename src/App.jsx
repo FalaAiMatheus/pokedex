7eb1cb1d94846bdab26fa5dd57195a006c70c4dc
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2023-11-20 11:49:19 -0300

    Estilizando a pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css'
 
 function App() {
   const [pokemon, setPokemon] = useState('');
@@ -22,16 +23,33 @@ function App() {
     <div className='container d-flex flex-column gap-3'>
       <div className="content">
       <div className="pokedex-container container-fluid bg-danger d-flex align-items-center flex-column p-5 rounded gap-3">
-        <div className="search-pokemon">
-          <h1>Pokédex</h1>
-          <input type="text" value={pokemon} onChange={handleInputChange} />
-          <button onClick={handleSearchClick}>Procurar</button>
+          <h1 className='text-white text-center'>Pokédex</h1>
+        <div className="search-pokemon d-flex gap-2">
+          <input type="text" className='input-search' value={pokemon} onChange={handleInputChange} />
+          <button className='btn btn-dark' onClick={handleSearchClick}>Procurar</button>
         </div>
         {pokemonData && (
           <div>
-            <h2>{pokemonData.id}</h2>
-            <h2>{pokemonData.name}</h2>
+            <div className="poke-info d-flex gap-2">
+              <h2 className='text-white'>#0{pokemonData.id}</h2>
+              <h2 className='text-capitalize text-white'>{pokemonData.name}</h2>
+            </div>
             <img src={pokemonData.sprites.other.dream_world.front_default} className='img-poke' alt={pokemonData.name} />
+            <div className="d-flex gap-4">
+              <h3><span class="badge bg-secondary">{pokemonData.types[0].type.name}</span></h3>
+              <h3><span class="badge bg-secondary">{pokemonData.types[1].type.name}</span></h3>
+            </div>
+            <div className="stats-poke">
+               <div class="stat">
+                  <span class="border border-black p-2 rounded bg-danger-subtle">HP: {pokemonData.stats[0].base_stat}</span>
+              </div>
+              <div class="stat">
+                  <span class="border border-black p-2 rounded bg-danger">ATK: {pokemonData.stats[1].base_stat}</span>
+              </div>
+              <div class="stat">
+                  <span class="border border-black p-2 rounded bg-info-subtle">DEF: {pokemonData.stats[2].base_stat}</span>
+              </div>
+            </div>
           </div>
         )}
       </div>
