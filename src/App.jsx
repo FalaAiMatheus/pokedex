7e7186cf7340c660de17a887c3e595eb0b6333e9
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2023-11-20 13:17:46 -0300

    Refatorando o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,28 @@ function App() {
 
   const handleInputChange = (event) => {
     setPokemon(event.target.value);
-  };
+}
 
-  const handleSearchClick = async () => {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-      const data = await response.json();
-      setPokemonData(data);
-    } catch (error) {
-      console.error("Erro ao buscar dados do Pokémon:", error);
-    }
-  };
+const handleSearchClick = async () => {
+  try{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await response.json();
+    setPokemonData(data);
+    console.log(data)
+  }
+  catch (error) {
+    console.error("Erro ao buscar dados do Pokémon:", error);
+  }
+}
 
-  return (
-    <div className='container d-flex flex-column gap-3'>
-      <div className="content">
+return (
+  <div className="container d-flex flex-column gap-3">
+    <div className="content">
       <div className="pokedex-container container-fluid bg-danger d-flex align-items-center flex-column p-5 rounded gap-3">
-          <h1 className='text-white text-center'>Pokédex</h1>
+          <h1 className="text-white text-center" lang="en-US">Pokédex</h1>
         <div className="search-pokemon d-flex gap-2">
-          <input type="text" className='input-search' value={pokemon} onChange={handleInputChange} />
-          <button className='btn btn-dark' onClick={handleSearchClick}>Procurar</button>
+          <input type="text" id="input-pokemon" className="input-search" value={pokemon} onChange={handleInputChange} placeholder="Digite um ID ou Nome do Pokémon" />
+          <button className="btn btn-dark" onClick={handleSearchClick}>Procurar</button>
         </div>
         {pokemonData && (
           <div>
@@ -36,17 +38,17 @@ function App() {
             </div>
             <img src={pokemonData.sprites.other.dream_world.front_default} className='img-poke' alt={pokemonData.name} />
             <div className="d-flex gap-4">
-              <h3><span class="badge bg-secondary text-uppercase">{pokemonData.types[0].type.name}</span></h3>
+              <h3><span className="badge bg-secondary text-uppercase">{pokemonData.types[0].type.name}</span></h3>
             </div>
             <div className="stats-poke">
-               <div class="stat">
-                  <span class="border border-black p-2 rounded bg-danger-subtle text-uppercase">Hp: {pokemonData.stats[0].base_stat}</span>
+               <div className="stat">
+                  <span className="border border-black p-2 rounded optimize fw-bold text-uppercase">Hp: {pokemonData.stats[0].base_stat}</span>
               </div>
-              <div class="stat">
-                  <span class="border border-black p-2 rounded bg-danger-subtle text-uppercase">Atk: {pokemonData.stats[1].base_stat}</span>
+              <div className="stat">
+                  <span className="border border-black p-2 rounded optimize fw-bold text-uppercase">Atk: {pokemonData.stats[1].base_stat}</span>
               </div>
-              <div class="stat">
-                  <span class="border border-black p-2 rounded bg-info-subtle text-uppercase">Def: {pokemonData.stats[2].base_stat}</span>
+              <div className="stat">
+                  <span className="border border-black p-2 rounded optimize fw-bold text-uppercase">Def: {pokemonData.stats[2].base_stat}</span>
               </div>
             </div>
           </div>
